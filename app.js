f45ff4991f5fc8c10f006fcf2127ca875f9242c6
John Phillips <johnphillips.dev@gmail.com>
2021-09-29 14:13:37 +0100

    Sections slide in and out.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const profileSection = document.querySelector('.section-content');
 const projectSection = document.querySelector('.project-section');
 
+const mainNav = document.querySelectorAll('.nav-item');
+
 const isNavClosing = (bool) => {
     const navbar = document.querySelector('.navbar');
     const navLists = document.querySelectorAll('.nav-list');
@@ -13,8 +15,13 @@ const isNavClosing = (bool) => {
         navbar.style.height = '0px';
         setTimeout(() => {
             crossIcon.style.opacity = '0';
-            profileSection.style.display = 'block';
-            projectSection.style.display = 'block';
+            if (mainNav[0].classList.contains('active')) {
+                profileSection.style.display = 'block';
+                projectSection.style.display = 'none';
+            } else {
+                profileSection.style.display = 'none';
+                projectSection.style.display = 'block';
+            }
             menuIcon.style.display = 'block';
             navLists.forEach(item => {
                 item.style.transform = 'translateX(-768px)';
@@ -23,13 +30,17 @@ const isNavClosing = (bool) => {
         setTimeout(() => {
             navbar.style.display = 'none';
             menuIcon.style.opacity = '1';
-            profileSection.style.transform = 'translateX(0px)';
-            projectSection.style.transform = 'translateX(0px)';
+            if (mainNav[0].classList.contains('active')) {
+                profileSection.style.transform = 'translateX(0px)';
+            } else {
+                projectSection.style.transform = 'translateX(0px)';
+            }
         }, 1500);
     } else {
         // THE NAV IS OPENING!
         profileSection.style.transform = 'translateX(-768px)';
         projectSection.style.transform = 'translateX(-768px)';
+
         menuIcon.style.opacity = '0';
         navLists.forEach(item => {
             item.style.display = 'flex';
@@ -52,9 +63,6 @@ const isNavClosing = (bool) => {
 }
 
 const switchSection = (value) => {
-    const mainNav = document.querySelectorAll('.nav-item');
-
-
     if (!mainNav[value].classList.contains('active')) {
         if (mainNav[value - 1] === undefined) {
             mainNav[value].classList.add('active');
