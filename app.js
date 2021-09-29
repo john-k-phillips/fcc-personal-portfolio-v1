6eaaf2c6dacd3ffffb704d5707c5718b2b292cd2
John Phillips <johnphillips.dev@gmail.com>
2021-09-29 13:37:06 +0100

    Added switching navigation items.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const profileSection = document.querySelector('.section-content');
+const projectSection = document.querySelector('.project-section');
+
 const isNavClosing = (bool) => {
     const navbar = document.querySelector('.navbar');
     const navLists = document.querySelectorAll('.nav-list');
@@ -5,14 +8,13 @@ const isNavClosing = (bool) => {
     const crossIcon = document.querySelector('.close');
     const menuIcon = document.querySelector('.menu');
 
-    const sectionContent = document.querySelector('.section-content');
-
     if (bool) {
         // IS THE NAV CLOSING?
         navbar.style.height = '0px';
         setTimeout(() => {
             crossIcon.style.opacity = '0';
-            sectionContent.style.display = 'block';
+            profileSection.style.display = 'block';
+            projectSection.style.display = 'block';
             menuIcon.style.display = 'block';
             navLists.forEach(item => {
                 item.style.transform = 'translateX(-768px)';
@@ -21,11 +23,13 @@ const isNavClosing = (bool) => {
         setTimeout(() => {
             navbar.style.display = 'none';
             menuIcon.style.opacity = '1';
-            sectionContent.style.transform = 'translateX(0px)';
+            profileSection.style.transform = 'translateX(0px)';
+            projectSection.style.transform = 'translateX(0px)';
         }, 1500);
     } else {
         // THE NAV IS OPENING!
-        sectionContent.style.transform = 'translateX(-768px)';
+        profileSection.style.transform = 'translateX(-768px)';
+        projectSection.style.transform = 'translateX(-768px)';
         menuIcon.style.opacity = '0';
         navLists.forEach(item => {
             item.style.display = 'flex';
@@ -37,11 +41,27 @@ const isNavClosing = (bool) => {
         }, 10);
         setTimeout(() => {
             crossIcon.style.opacity = '1';
-            sectionContent.style.display = 'none';
+            profileSection.style.display = 'none';
+            projectSection.style.display = 'none';
             navLists.forEach(item => {
                 item.style.transform = 'translateX(0px)';
             })
             menuIcon.style.display = 'none';
         }, 500);
+    }
+}
+
+const switchSection = (value) => {
+    const mainNav = document.querySelectorAll('.nav-item');
+
+
+    if (!mainNav[value].classList.contains('active')) {
+        if (mainNav[value - 1] === undefined) {
+            mainNav[value].classList.add('active');
+            mainNav[value + 1].classList.remove('active');
+        } else if (mainNav[value + 1] === undefined) {
+            mainNav[value].classList.add('active');
+            mainNav[value - 1].classList.remove('active');
+        }
     }
 }
